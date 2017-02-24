811e6b44e753ac7dcad2b103f774abb5bf473fcb
Kazu Yokomizo <kaz@maisin.co>
2017-02-24 14:42:34 +0900

    Edited timer

--- src/main/timer/TimerComponent.js
+++ src/main/timer/TimerComponent.js
@@ -2,47 +2,48 @@ import React from 'react';
 import './Timer.css';
 
 export default React.createClass({
-  getInitialState() {
-    return {
+  getInitialState(){
+    return{
       elapsed: 0,
       running: false
     }
   },
-  handleStartClick() {
+  handleStartClick(){
     clearInterval(this.interval);
     this.interval = setInterval(this.updateElapsed, 100);
     this.setState({running: true});
   },
-  updateElapsed() {
+  updateElapsed(){
     this.setState({elapsed: this.state.elapsed + 1 });
   },
-  handleStopClick() {
+  handleStopClick(){
     clearInterval(this.interval);
     this.setState({running: false});
   },
-  handleResetClick() {
+  handleResetClick(){
     this.setState({
       elapsed: 0
     });
   },
-  getSeconds(elapsed) {
+  getSeconds(elapsed){
     return elapsed / 10 + (elapsed % 10 > 0 ? '' : '.0');
   },
-  render() {
+  render(){
     var _this = this;
-    return (
+    return(
       <div>
         <p>time: {this.getSeconds(this.state.elapsed)}</p>
-        {(this.state.running)
+        {
+          (this.state.running)
           ? <button onClick={this.handleStopClick}>Pause</button>
           : <button onClick={this.handleStartClick}>Start</button>
         }
-        {(this.state.elapsed > 0 && !this.state.running)
+        {
+          (this.state.elapsed > 0 && !this.state.running)
           ? <button onClick={this.handleResetClick}>Reset</button>
           : <button></button>
         }
       </div>
-
     );
   }
 });
